f7268c6152c1eb0a3e4cbf9b8bd88a3410328c2d
H193-C <114054482+H193-C@users.noreply.github.com>
2023-11-15 11:27:43 +0100

    ejercicio 1 resuelto

--- main.py
+++ main.py
@@ -7,13 +7,31 @@ Exercise 3: Basic calculator
 import random
 
 def guess_the_number():
-  """
+    """
     Using loops, implement a guessing game.
     Guess the number (1-10):
     messages: Too low, Too high, Try again, Congratulations!
-  """
-  # fix code
-  print("Guess the number (1-10):")
+    """
+    # Generate a random number between 1 and 10
+    secret_number = random.randint(1, 10)
+
+    while True:
+        # Get user input
+        guess = int(input("Enter your guess (1-10): "))
+
+        # Check if the guess is correct
+        if guess == secret_number:
+            print("Congratulations! You guessed the correct number.")
+            break
+        elif guess < secret_number:
+            print("Too low. Try again.")
+        else:
+            print("Too high. Try again.")
+
+# Call the function to start the game
+guess_the_number()
+
+
 
 
 def multiplication_table():
